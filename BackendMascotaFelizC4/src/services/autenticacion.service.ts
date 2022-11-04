f356f88283510a8b6a45558bd38274073d153b49
Javier Ortiz <javiortizo@hotmail.com>
2022-11-04 06:47:31 -0500

    Generación clave  de ingreso de usuarioy Notificación a usuario por correo elctrónico subibo por Javier Ortiz

--- BackendMascotaFelizC4/src/services/autenticacion.service.ts
+++ BackendMascotaFelizC4/src/services/autenticacion.service.ts
@@ -1,5 +1,6 @@
 import {injectable, /* inject, */ BindingScope} from '@loopback/core';
-
+const generador = require("password-generator");
+const cryptoJS = require("crypto-js");
 @injectable({scope: BindingScope.TRANSIENT})
 export class AutenticacionService {
   constructor(/* Add @inject to inject parameters */) {}
@@ -7,4 +8,16 @@ export class AutenticacionService {
   /*
    * Add service methods here
    */
+
+//Método para generar clave del usuario
+GenerarClave(){
+  let clave = generador(8, false); //Aquí se define la longitud de la contraseña y la complejidad de esta.
+  return clave;
+}
+
+//Método para cifrar clave
+CifrarClave(clave:string){
+  let claveCifrada = cryptoJS.MD5(clave).toString();
+  return claveCifrada;
+}
 }
